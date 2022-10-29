34605ffc5a28bc341b148eb2c749052274f10919
DuanNguyen <duannguyen242@gmail.com>
2022-10-30 00:13:39 +0700

    add view productCatgories, handle viewcart

---
+++ src/main/java/com/example/demo/responsitory/itemCartRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.responsitory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demo.entity.itemCart;
+
+public class itemCartRepository {
+	public itemCart item;
+	
+	
+	public void create() {
+		List<itemCart> list = new ArrayList<>();
+		
+	}
+
+}
