8623678ad43bc85c490b2eff97aac0c98adb88ae
DuanNguyen <duannguyen242@gmail.com>
2022-10-30 00:42:41 +0700

    add view productCate, handle viewcart

--- src/main/java/com/example/demo/entity/itemCart.java
+++ src/main/java/com/example/demo/entity/itemCart.java
@@ -12,9 +12,8 @@ public class itemCart {
 private int id;
 private String name;
 private String description;
+private double price;
 private String categories;
 private int quantity;
-	
-	
-	
+
 }
--- src/main/java/com/example/demo/responsitory/itemCartRepository.java
+++ src/main/java/com/example/demo/responsitory/itemCartRepository.java
@@ -1,17 +1,50 @@
 package com.example.demo.responsitory;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.example.demo.entity.itemCart;
 
 public class itemCartRepository {
-	public itemCart item;
 	
 	
-	public void create() {
-		List<itemCart> list = new ArrayList<>();
+	Map<Integer, itemCart> map = new HashMap<Integer,itemCart>();
+	
+	
+	public void add( itemCart item) {
+		itemCart exsistItemCart = map.get(item.getId());
+		
+		if (exsistItemCart != null) {
+			exsistItemCart.setQuantity(exsistItemCart.getQuantity() + item.getQuantity());
+		}
+		map.put(item.getId(),item);
 		
 	}
+	public void remove(int id) {
+		map.remove(id);
+	}
+	
+	public void clear() {
+		map.clear();
+	}
+	public Collection<itemCart> getList (){
+		return map.values();
+	}
 
+	public void update( int productId, int quantity) {
+		itemCart item = map.get(productId);
+		
+		item.setQuantity(quantity + item.getQuantity());
+		if (item.getQuantity() <= 0) {
+         
+			map.remove(productId);
+		}
+	}
+	
+	public double getAmount() {
+		
+	return	map.values().stream().mapToDouble(item -> item.getQuantity()* item.getPrice() ).sum();
+	}
 }
